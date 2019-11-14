5ebce408bb7b68680ae832d773da6f5ae475a16b
Alice Won <alicewon4@gmail.com>
2019-11-14 08:32:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def map(source_array)
+  #map returns an array with all values made negative
+  new_array = []
+  i = 0
+  while i < source_array.count do
+    #new_array<<source_array.[i] * -1
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  new_array
+end
